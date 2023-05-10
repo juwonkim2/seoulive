1762c10468beed8f3d8aabfcb4392dd963765bc4
dlehddud60 <dlehddud60@naver.com>
2023-05-10 15:33:59 +0900

    프로젝트 파일 배포

---
+++ src/main/java/org/zerock/myapp/controller/testController.java
@@ -0,0 +1,13 @@
+package org.zerock.myapp.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class testController {
+
+    @RequestMapping("/")
+    public String test1() {
+        return "board/home";
+    }
+}
